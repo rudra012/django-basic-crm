0fdcce2f060555cac901b71d8dc676040f1297df
Shailesh Rudra <shailesh@lintelindia.com>
2016-01-27 17:34:37 +0530

    SupremeApp Model is created

--- Supreme/settings.py
+++ Supreme/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'SupremeApp',
 )
 
 MIDDLEWARE_CLASSES = (
--- SupremeApp/admin.py
+++ SupremeApp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from SupremeApp.models import SupremeModel
+
+admin.site.register(SupremeModel)

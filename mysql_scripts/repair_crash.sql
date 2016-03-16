04f912287d2795bf4ff9130526dff2d4030ae84a
Shailesh Rudra <shailesh@lintelindia.com>
2016-03-16 12:09:07 +0530

    query added for crash mysql table at unwanted restart

--- mysql_scripts/repair_crash.sql
+++ mysql_scripts/repair_crash.sql
@@ -1,2 +1,12 @@
-USE supreme;
+USE supreme_new;
 REPAIR TABLE SupremeApp_suprememodel;
+USE supreme_new;
+REPAIR TABLE SupremeApp_suprememodel;
+
+--+-------------------------------------+--------+----------+-------------------------------------------------------+
+--| Table                               | Op     | Msg_type | Msg_text                                              |
+--+-------------------------------------+--------+----------+-------------------------------------------------------+
+--| supreme_new.SupremeApp_suprememodel | repair | info     | Found block with too small length at 4609736; Skipped |
+--| supreme_new.SupremeApp_suprememodel | repair | warning  | Number of rows changed from 7078 to 7077              |
+--| supreme_new.SupremeApp_suprememodel | repair | status   | OK                                                    |
+--+-------------------------------------+--------+----------+-------------------------------------------------------+

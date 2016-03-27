9484cf156663ea43b0c6c7e65bb655110c1daca9
Shailesh Rudra <shailesh@lintelindia.com>
2016-03-27 23:23:04 +0530

    restrictions on search fields for none superuser

--- SupremeApp/admin.py
+++ SupremeApp/admin.py
@@ -1,6 +1,9 @@
 import datetime
+
+import operator
 from django.conf import settings
 from django.contrib import admin
+from django.contrib.admin.utils import lookup_needs_distinct
 from django.db import models
 from string import join
 from .models import *
@@ -115,6 +118,48 @@ class SupremeAdmin(admin.ModelAdmin):
                    'allocation_date', ('account_balance', ValueRangeFilter), 'status']
 
     # save_on_top = True
+
+    def get_search_results(self, request, queryset, search_term):
+        """
+        Returns a tuple containing a queryset to implement the search,
+        and a boolean indicating if the results may contain duplicates.
+        """
+        # Apply keyword searches.
+        def construct_search(field_name):
+            if field_name.startswith('^'):
+                return "%s__istartswith" % field_name[1:]
+            elif field_name.startswith('='):
+                return "%s__iexact" % field_name[1:]
+            elif field_name.startswith('@'):
+                return "%s__search" % field_name[1:]
+            else:
+                return "%s__icontains" % field_name
+
+        use_distinct = False
+        search_fields = self.get_search_fields(request)
+        if search_fields and search_term:
+            orm_lookups = [construct_search(str(search_field))
+                           for search_field in search_fields]
+            for bit in search_term.split():
+                or_queries = [models.Q(**{orm_lookup: bit})
+                              for orm_lookup in orm_lookups]
+                queryset = queryset.filter(reduce(operator.or_, or_queries))
+            if not use_distinct:
+                for search_spec in orm_lookups:
+                    if lookup_needs_distinct(self.opts, search_spec):
+                        use_distinct = True
+                        break
+
+        if request.user.is_superuser:
+            return queryset, use_distinct
+        upload_history = UploadFileHistory.objects.filter(upload_type="D", generate_new_report=True).order_by("-uploaded_date")
+        if upload_history:
+            uploaded_date = datetime.datetime.strftime(upload_history[0].uploaded_date, '%Y-%m-%d %H:%M:%S')
+            queryset = queryset.filter(final_tc_name=request.user).filter(~Q(status="Paid"), date_created__gte = uploaded_date)
+        else:
+            queryset = queryset.filter(final_tc_name=request.user).filter(~Q(status="Paid"))
+
+        return queryset, use_distinct
 
     def save_formset(self, request, form, formset, change):
         instances = formset.save(commit=False)
